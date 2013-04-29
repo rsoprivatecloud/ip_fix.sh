222867c52772750742f08d358a199a6d32df186c
Ramsey Pawlik <ramsey.pawlik@rackspace.com>
2013-04-28 20:40:42 -0500

    Update ip_fix.sh

--- ip_fix.sh
+++ ip_fix.sh
@@ -8,12 +8,9 @@ GATEWAY=$(route -n | awk '/UG/ {print $2}')
 #remove old dhcp entries
 sed -i  '9,10d' /target/etc/network/interfaces
 
-cat >> /etc/network/interfaces <<EOF
+cat >> /target/etc/network/interfaces <<EOF
 iface eth0 inet static
         address $IP
         netmask $MASK
         gateway $GATEWAY
 EOF
-
-/etc/init.d/networking restart
-ifup eth0
