9fb070b52dad13ad1b7c45340f5e41edea80e175
Ramsey Pawlik <ramsey.pawlik@rackspace.com>
2013-04-28 20:34:38 -0500

    added script

---
+++ ip_fix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IP=$(hostname -I)
+IPMASK=$(ip a | grep $IP | awk '{print $2}')
+MASK=$(ipcalc $IPMASK | awk '/Netmask/ {print $2}')
+GATEWAY=$(route -n | awk '/UG/ {print $2}')
+
+#remove old dhcp entries
+sed -i  '9,10d' /etc/network/interfaces
+
+cat >> /etc/network/interfaces <<EOF
+iface eth0 inet static
+        address $IP
+        netmask $MASK
+        gateway $GATEWAY
+EOF
+
+/etc/init.d/networking restart
+ifup eth0
